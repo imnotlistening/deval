118da71e46e0c924fd5ae7600d5a10f3b6fd1d17
alex <alex@eb8bc956-3474-43e7-b57f-cf94f3f57431>
2010-12-03 19:56:29 +0000

    Changed buildprep.sh again

--- src/buildprep.sh
+++ src/buildprep.sh
@@ -4,6 +4,6 @@
 # Extract the SVN version from this directory and use that as the version.
 #
 
-REVISION=`svn info svn://australia.student.rit.edu/deval | awk '/Revision/ { print $2; }'`
+REVISION=`svnversion`
 
 echo "REVISION = $REVISION" > config.mak
