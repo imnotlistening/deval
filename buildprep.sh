8da387f2c7e72e604c39bb5fff56e97a6ea51f04
alex <alex@eb8bc956-3474-43e7-b57f-cf94f3f57431>
2011-01-21 23:44:07 +0000

    Another sync

--- buildprep.sh
+++ buildprep.sh
@@ -4,6 +4,14 @@
 # Extract the SVN version from this directory and use that as the version.
 #
 
+svnversion -c . >.tmp.txt
+awk 'BEGIN { FS = ":" } { 
+if ( $2 ) 
+  print $2;
+else
+  print $1
+}' .tmp.txt
+
 REVISION=`svnversion .`
 
 echo "REVISION = $REVISION" > config.mak
